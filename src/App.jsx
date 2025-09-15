2f07d1483c349eb4254fbe80ce3fec19ecdedc5d
rodnulka 1 <mih80852@gmail.com>
2025-09-15 20:47:30 +0300

    add-new-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import './App.css';
 import Button from './components/Button/Button';
-import CheckBox from './components/CheckBox/CheckBox';
-import P from './components/P/P';
-import Scroll from './components/Scroll/Scroll';
+import CardButton from './components/CardButton/CardButton';
+import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
+	const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			text: 'Горные походы вместе с Павлом и нашей группой П23 в Сочи',
+			date: new Date()
+		},
+		{
+			title: 'Отдых  ',
+			text: 'Походы в кальяную с Павлом ',
+			date: new Date()
+		}
+	];
 	return (
 		<>
 			<Button />
-			<br />
-			<Scroll />
-			<br />
-			<P />
-			<br />
-			<CheckBox />
+			<CardButton>Новое воспоминание</CardButton>
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+			</CardButton>
+
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+			</CardButton>
 		</>
 	);
 }
