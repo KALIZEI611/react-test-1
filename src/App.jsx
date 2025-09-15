f44d00593d2b7eb30b52ac027e4727f465e7ff95
rodnulka 1 <mih80852@gmail.com>
2025-09-15 18:17:02 +0300

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CheckBox from './components/CheckBox/CheckBox';
+import P from './components/P/P';
+import Scroll from './components/Scroll/Scroll';
 
 
 function App() {
 	return (
 		<>
 			<Button/>
+			<br/>
+			<Scroll/>
+			<br/>
+			<P/>
+			<br/>
+			<CheckBox/>
 		</>
 	);
 }
