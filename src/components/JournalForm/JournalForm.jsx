a2fcd06e3f84ce43baea26e7aa2f190bdbb46ea5
Misha <mih80852@gmail.com>
2025-09-23 13:50:47 +0300

    stylization

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,22 +1,60 @@
 
 import './JournalForm.css';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 function JournalForm({onSubmit}) {
+
+    const [formValidState,setFormValidState]=useState({
+        title:true,
+        post:true,
+        date:true
+    });
 
     const addJournalItem = (e)=>{
         const formData= new FormData(e.target);
         const formProps = Object.fromEntries(formData);
+        let isValid = true;
         e.preventDefault();
+        if(!formProps.title?.trim().length){
+            setFormValidState(state=>({...state,title:false}));
+            isValid=false;
+        }
+        else{
+            setFormValidState(state=>({...state,title:true}));
+        }
+
+
+        if(!formProps.post?.trim().length){
+            setFormValidState(state=>({...state,post:false}));
+             isValid=false;
+        }
+        else{
+            setFormValidState(state=>({...state,post:true}));
+        }
+
+
+        if(!formProps.date){
+            setFormValidState(state=>({...state,date:false}));
+             isValid=false;
+        }
+        else{
+            setFormValidState(state=>({...state,date:true}));
+        }
+
+
+        if(!isValid){
+            return;
+        }
         onSubmit(formProps);
     }
     return (
         <form className='journalForm' onSubmit={addJournalItem}>
-            <input type="text" name='title'  />
-            <input type="date" name='date'/>
+            <input type="text" name='title' style={{border:formValidState.title ? undefined : "1px solid red"}}/>
+            <input type="date" name='date'style={{border:formValidState.date ? undefined : "1px solid red"}} />
             <input type="text" name='tag' />
-            <textarea cols="30" rows="10" name='text' />   
-            <Button text="Сохранить" onClick={()=>{console.log("Нажали");}} />  
+            <textarea name='post' cols="30" rows="10"  style={{border:formValidState.post ? undefined : "1px solid red"}}/>   
+            <Button text="Сохранить"/>  
         </form>
     );
 }
