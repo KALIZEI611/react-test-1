e451ab495c9f6cb5d74f6255fad02da452fb30ce
Misha <mih80852@gmail.com>
2025-09-25 13:56:40 +0300

    useEffect-48

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,32 @@ import Body from './layout/Body/Body';
 import LeftPanel from './layout/LeftPanel/LeftPanel';
 import './App.css';
 import JournalForm from './components/JournalForm/JournalForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const INITIAL_DATA = [
-		{
-			title: 'Подготовка к обновлению курсов',
-			text: 'Горные походы вместе с Павлом и нашей группой П23 в Сочи',
-			date: new Date(),
-			body:
-				'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere ipsam nobis quis quaerat blanditiis porro modi adipisci in cupiditate expedita inventore, quisquam iusto non vitae, minima assumenda architecto sit est!'
-		},
-		{
-			title: 'Отдых  ',
-			text: 'Походы в кальяную с Павлом ',
-			date: new Date()
-		}
-];
+
 
 function App() {
-	const [items,setItems]=useState(INITIAL_DATA);
+	const [items,setItems]=useState([]);
 	
+	useEffect(()=>{
+		const data = JSON.parse(localStorage.getItem('data'));
+		if(data)
+		{
+			setItems(data.map(item=>({
+				...item,
+				date:new Date(item.date)
+			})));
+		}
+	},[]);
+
+	useEffect(()=>{
+		if(items.length){
+			localStorage.setItem('data',JSON.stringify(items));
+		}
+	},[items])
 	const addItem=item =>{
 		setItems(oldItems => [...oldItems,{
-			text:item.text,
+			post:item.post,
 			title:item.title,
 			date:new Date(item.date),
 			id:oldItems.length>0 ? Math.max(...oldItems.map(i => i.id))+1:1
